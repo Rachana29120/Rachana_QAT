70a46c9648c47dec70136586ee9c869f89381913
Rachana29120 <57671126+Rachana29120@users.noreply.github.com>
2021-08-20 10:19:03 +0530

    Add files via upload

---
+++ MainApplication.java
@@ -0,0 +1,17 @@
+
+public class MainApplication {
+	public static void main(String[] args) 
+	{
+		CurrentAccount obj1=new CurrentAccount();
+		SavingsAccount obj2=new SavingsAccount();
+		obj1.checkbankbalance();
+		obj1.validateUser();
+		obj2.checkbankbalance();
+		obj2.validateUser();
+
+	}
+
+}
+	
+
+
